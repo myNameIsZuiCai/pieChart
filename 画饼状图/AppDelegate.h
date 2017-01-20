e3c11cfc317d0caacc36ae32b540829620959aaf
“shanghaijunheng” <78899500@qq.com>
2017-01-20 10:59:24 +0800

    Initial Commit

---
+++ 画饼状图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  画饼状图
+//
+//  Created by 上海均衡 on 2017/1/20.
+//  Copyright © 2017年 上海均衡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 画饼状图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  画饼状图
+//
+//  Created by 上海均衡 on 2017/1/20.
+//  Copyright © 2017年 上海均衡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
